71f1c902cfaa319b2b6fa329a81cfb344ec4b1a4
oneeyedziggy <oneeyedziggy@gmail.com>
2022-06-03 08:29:46 -0700

    got tests passing... now see if i can stick it in a lambdagot tests passing... now see if i can stick it in a lambda

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const window = new JSDOM('').window;
 const DOMPurify = createDOMPurify(window);
 
 const main = ( input ) => {
-    const result = DOMPurify.sanitize(input);
-    console.log('>', result, '< logged');
-    return result;
+    return DOMPurify.sanitize(input);
 };
-
-main("<html>hello<p></html>");
 
 export default main;
--- index.spec.js
+++ index.spec.js
@@ -1,15 +1,19 @@
 //import { describe, it, expect } from 'jest';
 
-import main from 'index';
+import main from './index';
 
 describe("main function", () => {
     it( "should take valid input and return valid output", () => {
-        const validHTML = "<html><head><title>things</title></head><body>stuff</body></html>";
-        expect( main(validHTML) ).toEqual(validHTML);
+        //const validHTML = "<html><head><title>things</title></head><body>stuff</body></html>";
+        //stuff
+        const validHTML = "<h1>stuff</h1><table><tr><td>cell1</td><td>cell2</td></tr></table>";
+        const moreValidHTML = "<h1>stuff</h1><table><tbody><tr><td>cell1</td><td>cell2</td></tr></tbody></table>";
+        
+        expect( main(validHTML) ).toEqual(moreValidHTML);
     } );
     it( "should take invalid input and return valid output", () => {
-        const invalidHTML = "<html><head><title>things</head><body>stuff</body></html>";
-        const validHTML = "<html><head></head><body>stuff</body></html>";
+        const invalidHTML = "<h1>stuff";
+        const validHTML = "<h1>stuff</h1>";
         expect( main(invalidHTML) ).toEqual(validHTML);
     } );
 });
